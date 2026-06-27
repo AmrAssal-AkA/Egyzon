f5f40b4b4d159dce67cfe8209ea8a5257bf4eb0e
AmrAssal_AkA <amrkhaledassal2024@gmail.com>
2026-06-28 01:04:12 +0300

    remvoe .next and .DS_Store

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"uliLRTu8f+wGSZ2QuMgUc5YF1JyHc+vBRuBaYkBRtVY=\"\n}"
--- .next/static/chunks/app_layout_jsx_05c78893._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_137b156b._.css",
-  "static/chunks/_8aeb1621._.js",
-  "static/chunks/node_modules_1621bf59._.js"
-],
-    source: "dynamic"
-});
