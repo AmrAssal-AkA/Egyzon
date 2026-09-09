c23710acef215719568cac87028da4012ccfdcce
AmrAssal_AkA <amrkhaledassal2024@gmail.com>
2026-09-09 13:27:56 +0300

    final Reegister fix

--- codeBase/server/src/controller/authentication/Register.ts
+++ codeBase/server/src/controller/authentication/Register.ts
@@ -52,7 +52,7 @@ const RegisterUser = async (userData: any, res: Response, req: Request) => {
     const verificationUrl = `${process.env.FRONTEND_URL}/verifyEmail?token=${emailTokenValue}`;
     // Send verification email
     try {
-      await verifyEmailTemplate(email, verificationUrl);
+      verifyEmailTemplate(email, verificationUrl);
     }catch (err) {
       logger.error("Error sending verification email:", err);
     }
